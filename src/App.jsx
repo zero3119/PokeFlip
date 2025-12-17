b57ef6f96028de492aab1da8ed63b5c7baf101e9
zero3119 <zamoranoadan43@gmail.com>
2025-12-16 18:00:47 -0700

    created React Vite aplication and added Tailwind to manage frontend easier

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-500 mx-auto">
+        <h1 className="text-8xl text-shadow-sky-700">akhjgtneasklrszjkg</h1>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
